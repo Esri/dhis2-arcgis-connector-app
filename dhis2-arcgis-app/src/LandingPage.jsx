8b854bbde87acf0f436ae9cf8687f563c094a158
Katharine Weintraub <kat14004@esri.com>
2025-01-07 14:36:36 -0500

    Made small tweaks to App.jsx imports, and refactored both the jsx and css for LandingPage to incorporate the Calcite components.

--- dhis2-arcgis-app/src/LandingPage.jsx
+++ dhis2-arcgis-app/src/LandingPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import './ArcGISPreSignInPage.css';
+import { CalciteButton, CalciteCard, CalciteLabel } from '@esri/calcite-components-react';
+import './LandingPage.css';
 
-const ArcGISPreSignIn = () => {
+const LandingPage = () => {
   const history = useHistory();
 
-  const handleClick = () => {
+  const handleSignInOnline = () => {
     history.push('/signin'); // Redirect to the sign in page
+  };
+
+  const handleSignInEnterprise = () => {
+    history.push('/signin-enterprise'); // Redirect to the ArcGIS Enterprise sign in page
   };
 
   return (
@@ -15,34 +20,27 @@ const ArcGISPreSignIn = () => {
       <div className="rectangle1"></div>
       <div className="dhis2-to-arcgis-connector-app">DHIS2 to ArcGIS Connector App</div>
       <div className="rectangle2"></div>
-      <div className="frame1">
-        <div className="dhis2-to-arcgis-connector">DHIS2 to ArcGIS Connector</div>
-        <div className="integrate-your-dhis2-data">
+      <CalciteCard className="frame1">
+        <CalciteLabel className="dhis2-to-arcgis-connector">
+          DHIS2 to ArcGIS Connector
+        </CalciteLabel>
+        <CalciteLabel className="integrate-your-dhis2-data">
           Integrate your DHIS2 data into your ArcGIS Online or ArcGIS Enterprise organization to enable seamless data sharing, analysis, and visualization.
-        </div>
-        <div className="sign-in-button">
-          <div className="button-container">
-            <div className="button" onClick={handleClick}>Sign In</div>
-          </div>
+        </CalciteLabel>
+        <div className="sign-in-buttons">
+          <CalciteButton className="button" onClick={handleSignInOnline}>
+            Sign In with ArcGIS Online
+          </CalciteButton>
+          <CalciteButton className="button" onClick={handleSignInEnterprise} appearance="outline">
+            Sign In with ArcGIS Enterprise
+          </CalciteButton>
         </div>
         <div className="arcgis-online">
-          <div className="logo">
-            <div className="path"></div>
-            <div className="clipped">
-              <div className="vector"></div>
-              <div className="path"></div>
-            </div>
-            <div className="clipped">
-              <div className="vector"></div>
-              <div className="path"></div>
-              <div className="vector"></div>
-            </div>
-            <div className="vector"></div>
-          </div>
+          <div className="logo"></div>
         </div>
-      </div>
+      </CalciteCard>
     </div>
   );
 };
 
-export default ArcGISPreSignIn;
+export default LandingPage;
