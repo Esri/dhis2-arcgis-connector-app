1ff0a1250c2ff177c16c333af2ea3d8b62ec8d87
Adam Pfister <apfister@esri.com>
2025-01-07 16:22:32 -0600

    routing checks with hash router

--- dhis2-arcgis-app/src/App.jsx
+++ dhis2-arcgis-app/src/App.jsx
@@ -1,6 +1,14 @@
 import { useDataQuery } from "@dhis2/app-runtime";
 import i18n from "@dhis2/d2-i18n";
 import React from "react";
+import {
+  BrowserRouter,
+  HashRouter,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
 import classes from "./App.module.css";
 import { setAssetPath } from "@esri/calcite-components/dist/components";
 
@@ -9,17 +17,23 @@ import "@esri/calcite-components/dist/components/calcite-button.js";
 import "@esri/calcite-components/dist/components/calcite-icon.js";
 import "@esri/calcite-components/dist/components/calcite-slider.js";
 import "@esri/calcite-components/dist/components/calcite-shell.js";
+import "@esri/calcite-components/dist/components/calcite-shell-panel.js";
+import "@esri/calcite-components/dist/components/calcite-panel.js";
 
 import "@esri/calcite-components/dist/calcite/calcite.css";
 import {
   CalciteButton,
   CalciteIcon,
+  CalcitePanel,
   CalciteShell,
   CalciteSlider,
 } from "@esri/calcite-components-react";
+import LandingPage from "./LandingPage";
 
 // Local assets
-setAssetPath(window.location.href);
+// setAssetPath(window.location.href);
+
+setAssetPath("https://unpkg.com/@esri/calcite-components/dist/calcite/assets");
 
 const query = {
   me: {
@@ -27,8 +41,10 @@ const query = {
   },
 };
 
-const MyApp = () => {
+// Create a separate component for the app content
+const AppContent = () => {
   const { error, loading, data } = useDataQuery(query);
+  const navigate = useNavigate();
 
   if (error) {
     return <span>{i18n.t("ERROR")}</span>;
@@ -43,15 +59,67 @@ const MyApp = () => {
       <div slot="header" style={{ height: "75px", border: "1px solid red" }}>
         app Header goes here
       </div>
-      {/* <div className={classes.container}>
-        <h1>{i18n.t("Hello {{name}}", { name: data.me.name })}</h1>
-        <h3>{i18n.t("Welcome to DHIS2!")}</h3>
-        <CalciteButton appearance="solid" color="blue" scale="m">
-          <CalciteIcon icon="plus" scale="s" />
-          {i18n.t("Add something")}
-        </CalciteButton>
-      </div> */}
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div
+              style={{
+                height: "100vh",
+                border: "3px solid green",
+                padding: "1.5rem",
+              }}
+            >
+              Landing Page goes here!
+              <CalciteButton
+                onClick={() => navigate("/connections")}
+                style={{ marginRight: "1rem" }}
+              >
+                Go to Connections
+              </CalciteButton>
+              <CalciteButton
+                onClick={() => navigate("/add-connection")}
+                style={{ marginRight: "1rem" }}
+              >
+                Go to Add Connection
+              </CalciteButton>
+            </div>
+          }
+        />
+        <Route
+          path="/connections"
+          element={
+            <div style={{ padding: "1.5rem" }}>
+              hello connections route
+              <CalciteButton onClick={() => navigate("/")}>
+                Back to Landing
+              </CalciteButton>
+            </div>
+          }
+        />
+        <Route
+          path="/add-connection"
+          element={
+            <div style={{ padding: "1.5rem" }}>
+              hello add connection route
+              <CalciteButton onClick={() => navigate("/")}>
+                Back to Landing
+              </CalciteButton>
+            </div>
+          }
+        />
+      </Routes>
     </CalciteShell>
+  );
+};
+
+// Main App component
+const MyApp = () => {
+  return (
+    <HashRouter>
+      <AppContent />
+    </HashRouter>
   );
 };
 
