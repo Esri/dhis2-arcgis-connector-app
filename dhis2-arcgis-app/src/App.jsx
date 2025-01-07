ad7a098c8815498ae3b8799aeed335e01a9cd5ba
Adam Pfister <apfister@esri.com>
2025-01-07 14:43:05 -0600

    quick layout update

--- dhis2-arcgis-app/src/App.jsx
+++ dhis2-arcgis-app/src/App.jsx
@@ -1,46 +1,58 @@
-import { useDataQuery } from '@dhis2/app-runtime';
-import i18n from '@dhis2/d2-i18n';
-import React from 'react';
-import classes from './App.module.css';
+import { useDataQuery } from "@dhis2/app-runtime";
+import i18n from "@dhis2/d2-i18n";
+import React from "react";
+import classes from "./App.module.css";
 import { setAssetPath } from "@esri/calcite-components/dist/components";
 
 // Calcite components
 import "@esri/calcite-components/dist/components/calcite-button.js";
 import "@esri/calcite-components/dist/components/calcite-icon.js";
 import "@esri/calcite-components/dist/components/calcite-slider.js";
+import "@esri/calcite-components/dist/components/calcite-shell.js";
+
 import "@esri/calcite-components/dist/calcite/calcite.css";
-import { CalciteButton, CalciteIcon, CalciteSlider } from "@esri/calcite-components-react";
+import {
+  CalciteButton,
+  CalciteIcon,
+  CalciteShell,
+  CalciteSlider,
+} from "@esri/calcite-components-react";
 
 // Local assets
 setAssetPath(window.location.href);
 
 const query = {
-    me: {
-        resource: 'me',
-    },
-}
+  me: {
+    resource: "me",
+  },
+};
 
 const MyApp = () => {
-    const { error, loading, data } = useDataQuery(query)
+  const { error, loading, data } = useDataQuery(query);
 
-    if (error) {
-        return <span>{i18n.t('ERROR')}</span>
-    }
+  if (error) {
+    return <span>{i18n.t("ERROR")}</span>;
+  }
 
-    if (loading) {
-        return <span>{i18n.t('Loading...')}</span>
-    }
+  if (loading) {
+    return <span>{i18n.t("Loading...")}</span>;
+  }
 
-    return (
-        <div className={classes.container}>
-            <h1>{i18n.t('Hello {{name}}', { name: data.me.name })}</h1>
-            <h3>{i18n.t('Welcome to DHIS2!')}</h3>
-            <CalciteButton appearance="solid" color="blue" scale="m">
-                <CalciteIcon icon="plus" scale="s" />
-                {i18n.t('Add something')}
-            </CalciteButton>
-        </div>
-    )
-}
+  return (
+    <CalciteShell>
+      <div slot="header" style={{ height: "75px", border: "1px solid red" }}>
+        app Header goes here
+      </div>
+      {/* <div className={classes.container}>
+        <h1>{i18n.t("Hello {{name}}", { name: data.me.name })}</h1>
+        <h3>{i18n.t("Welcome to DHIS2!")}</h3>
+        <CalciteButton appearance="solid" color="blue" scale="m">
+          <CalciteIcon icon="plus" scale="s" />
+          {i18n.t("Add something")}
+        </CalciteButton>
+      </div> */}
+    </CalciteShell>
+  );
+};
 
-export default MyApp
+export default MyApp;
