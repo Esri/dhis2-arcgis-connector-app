374d7c6a347b92beea1a04dee99b34e70f8caf7b
Katharine Weintraub <kat14004@esri.com>
2025-01-07 13:57:21 -0500

    Resolved Calcite issue, tested out Calcite button on App.jsx, and renamed LandingPage.

--- dhis2-arcgis-app/src/App.jsx
+++ dhis2-arcgis-app/src/App.jsx
@@ -1,7 +1,19 @@
-import { useDataQuery } from '@dhis2/app-runtime'
-import i18n from '@dhis2/d2-i18n'
-import React from 'react'
-import classes from './App.module.css'
+import { useDataQuery } from '@dhis2/app-runtime';
+import i18n from '@dhis2/d2-i18n';
+import React from 'react';
+import classes from './App.module.css';
+import { setAssetPath } from "@esri/calcite-components/dist/components";
+// define the custom elements on the browser
+import "@esri/calcite-components/dist/components/calcite-button.js";
+import "@esri/calcite-components/dist/components/calcite-icon.js";
+import "@esri/calcite-components/dist/components/calcite-slider.js";
+import "@esri/calcite-components/dist/calcite/calcite.css";
+
+// import the React wrapper components
+import { CalciteButton, CalciteIcon, CalciteSlider } from "@esri/calcite-components-react";
+
+// Local assets
+setAssetPath(window.location.href);
 
 const query = {
     me: {
@@ -24,6 +36,10 @@ const MyApp = () => {
         <div className={classes.container}>
             <h1>{i18n.t('Hello {{name}}', { name: data.me.name })}</h1>
             <h3>{i18n.t('Welcome to DHIS2!')}</h3>
+            <CalciteButton appearance="solid" color="blue" scale="m">
+                <CalciteIcon icon="plus" scale="s" />
+                {i18n.t('Add something')}
+            </CalciteButton>
         </div>
     )
 }
