528a438307786efa1a11f2cb5d5d6455419dc63e
Katharine Weintraub <kat14004@esri.com>
2025-01-08 17:03:33 -0500

    Brought in existing DHIS2 org unit tree that has worked on other DHIS2 apps; will be editing this for purpose of our app.

---
+++ dhis2-arcgis-app/src/components/OrgUnitTree.jsx
@@ -0,0 +1,39 @@
+// Brought in existing code to start this off. 
+// TODO: Need to update the code to work for the Create New Connection page.
+import { useNavigate } from "react-router-dom";
+import { OrganisationUnitTree } from "@dhis2/ui";
+import exploreStore from "../../store/exploreStore";
+import useOrgUnitRoots from "../../hooks/useOrgUnitRoots";
+import styles from "./styles/OrgUnitTree.module.css";
+
+const OrgUnitTree = () => {
+  const { orgUnit } = exploreStore();
+  const { roots } = useOrgUnitRoots();
+  const navigate = useNavigate();
+  const path = orgUnit?.path.split("/");
+
+  const initiallyExpanded =
+    path?.length > 2
+      ? [path.slice(0, -1).join("/")]
+      : roots?.map((r) => r.path);
+
+  const selected = orgUnit?.path ? [orgUnit.path] : [];
+
+  const onChange = (orgUnit) => navigate(`/explore/${orgUnit.id}`);
+
+  return roots ? (
+    <div className={styles.container}>
+      <div className={styles.orgUnitTree}>
+        <OrganisationUnitTree
+          roots={roots.map((r) => r.id)}
+          selected={selected}
+          onChange={onChange}
+          singleSelection={true}
+          initiallyExpanded={initiallyExpanded}
+        />
+      </div>
+    </div>
+  ) : null;
+};
+
+export default OrgUnitTree;
