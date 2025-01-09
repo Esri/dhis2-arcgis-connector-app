b6c287914fe2d6a30d71a7d998cdc301bead2b00
Katharine Weintraub <kat14004@esri.com>
2025-01-09 15:38:02 -0500

    Removed the custom breadcrumb to utilize existing Calcite Stepper component in NewConnection.jsx

--- dhis2-arcgis-app/src/pages/NewConnection.jsx
+++ dhis2-arcgis-app/src/pages/NewConnection.jsx
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import OrgUnitTree from "../components/OrgUnitTree";
-import Breadcrumb from "../components/NewConnectBreadcrumb";
-
-const steps = [
-  "Organisation units",
-  "Period",
-  "Events",
-  "Aggregation",
-  "Instance Selection",
-  "Summary",
-];
+import {
+  CalciteButton,
+  CalciteCard,
+  CalciteLabel,
+  CalciteStepper,
+  CalciteStepperItem,
+  CalciteNotice,
+} from "@esri/calcite-components-react";
 
 const NewConnection = () => {
+  // const [currentStep, setCurrentStep] = useState(0);
+
   return (
-  <div style={{ padding: "1rem", height: "100vh" }}>
-    <Breadcrumb steps={steps} currentStep={currentStep} />
-    <OrgUnitTree />
+    <div style={{ padding: "1rem", height: "100vh" }}>
+      <CalciteStepper numbered>
+        <CalciteStepperItem 
+        heading="Organisation units"
+        >
+          <CalciteNotice 
+          width="full" 
+          open
+          >
+            <div slot="title">Step 1 Content</div>
+          </CalciteNotice>
+        </CalciteStepperItem>
+        <CalciteStepperItem heading="Period">
+        <CalciteNotice width="full" open>
+            <div slot="title">Step 2 Content</div>
+          </CalciteNotice>
+        </CalciteStepperItem>
+        <CalciteStepperItem heading="Events">
+          <CalciteNotice width="full" open>
+            <div slot="title">Step 3 Content</div>
+          </CalciteNotice>
+        </CalciteStepperItem>
+        <CalciteStepperItem heading="Aggregation">
+          <CalciteNotice width="full" open>
+            <div slot="title">Step 4 Content</div>
+          </CalciteNotice>
+        </CalciteStepperItem>
+        <CalciteStepperItem heading="Instance Selection">
+          <CalciteNotice width="full" open>
+            <div slot="title">Step 5 Content</div>
+          </CalciteNotice>
+        </CalciteStepperItem>
+        <CalciteStepperItem heading="Summary">
+          <CalciteNotice width="full" open>
+            <div slot="title">Step 6 Content</div>
+          </CalciteNotice>
+        </CalciteStepperItem>
+      </CalciteStepper>
+      <OrgUnitTree />
     </div>
   );
 };
