cddedd78fbf5f652440a78970c2a76c500d99436
Katharine Weintraub <kat14004@esri.com>
2025-01-10 08:41:02 -0500

    Issue #18: Swapped 'Create New Connection' and 'Manage Connections' on the SubHeader.

--- dhis2-arcgis-app/src/components/SubHeader.jsx
+++ dhis2-arcgis-app/src/components/SubHeader.jsx
@@ -23,18 +23,18 @@ const SubHeader = () => {
     >
       <CalciteMenu slot="content-start">
         <CalciteMenuItem
-          text={i18n.t("Manage Connections")}
-          textEnabled
-          onClick={() => navigate("/connections", { replace: true })}
-          active={window.location.hash === "#/connections" ? true : undefined}
-        ></CalciteMenuItem>
-        <CalciteMenuItem
           text={i18n.t("Create New Connection")}
           textEnabled
           onClick={() => navigate("/add-connection", { replace: true })}
           active={
             window.location.hash === "#/add-connection" ? true : undefined
           }
+        ></CalciteMenuItem>
+        <CalciteMenuItem
+          text={i18n.t("Manage Connections")}
+          textEnabled
+          onClick={() => navigate("/connections", { replace: true })}
+          active={window.location.hash === "#/connections" ? true : undefined}
         ></CalciteMenuItem>
       </CalciteMenu>
     </CalciteNavigation>
